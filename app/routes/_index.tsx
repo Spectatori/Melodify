0678f646dabf45bd5ab92ecec73601501595c216
Spectatori <cavdar2002@gmail.com>
2025-01-26 17:39:19 +0200

    feat: add navigation to login on Get Started button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import Background from "../../public/IndexPage/background.jpg";
+import Background from "/IndexPage/background.jpg";
+import { useNavigate } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,6 +9,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const navigate = useNavigate();
   return (
     <div className="flex w-full h-max bg-gradient-to-b from-secondary to-primary flex-col">
       <div className="flex items-center pt-14 flex-col">
@@ -15,12 +17,11 @@ export default function Index() {
         <h2 className="pt-10 text-2xl text-primary">Melodify is a unique and robust web app that let’s you choose your vibe and creates the Spotify playlist for you.</h2>
       </div>
       <div className="flex items-center justify-center pt-20">
-        <button className="bg-black p-5 text-primary rounded-sm text-xl">Get Started</button>
+        <button className="bg-black p-5 text-primary rounded-sm text-xl" onClick={() => navigate("/login")}>Get Started</button>
       </div>
       <div className="flex justify-center pt-16 pb-20">
         <img src={Background} className="w-1/2"/>
       </div>
-
     </div>
   );
 }
