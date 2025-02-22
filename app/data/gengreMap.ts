bb23ff34567c7dcd9383519eef92a4cdbd1f9d1a
Spectatori <cavdar2002@gmail.com>
2025-02-22 15:22:40 +0200

    feat: add genre mapping data for music categorization

---
+++ app/data/gengreMap.ts
@@ -0,0 +1,15 @@
+export const genreMap = {
+  'Rock': ['Alternative', 'Classic Rock', 'Indie Rock', 'Progressive Rock', 'Punk Rock', 'Soft Rock'],
+  'Pop': ['Dance Pop', 'Indie Pop', 'K-pop', 'Synth-pop', 'Pop Rock', 'Traditional Pop'],
+  'Hip-Hop': ['Trap', 'Rap', 'Old School', 'Underground', 'Alternative Hip Hop', 'Pop Rap'],
+  'Country': ['Traditional', 'Modern', 'Bluegrass', 'Country Pop', 'Country Rock', 'Western'],
+  'Classical': ['Baroque', 'Classical Period', 'Romantic', 'Contemporary', 'Opera', 'Chamber Music'],
+  'Electro': ['House', 'Techno', 'Trance', 'Dubstep', 'Ambient', 'EDM'],
+  'Jazz': ['Bebop', 'Swing', 'Cool Jazz', 'Free Jazz', 'Fusion', 'Latin Jazz'],
+  'Latin': ['Salsa', 'Reggaeton', 'Bachata', 'Latin Pop', 'Latin Rock', 'Merengue'],
+  'Heavy Metal': ['Black Metal', 'Death Metal', 'Thrash Metal', 'Power Metal', 'Nu Metal', 'Gothic Metal'],
+  'K-pop': ['Boy Groups', 'Girl Groups', 'Solo Artists', 'K-rap', 'K-rock', 'K-indie'],
+  'Funk': ['P-Funk', 'Deep Funk', 'Funk Rock', 'G-Funk', 'Jazz-Funk', 'Soul Funk'],
+  'Folk': ['Traditional', 'Contemporary', 'Celtic', 'American', 'Folk Rock', 'World Folk'],
+  'Gym': ['Workout', 'Power Music', 'High Energy', 'Cardio', 'Training', 'Motivation']
+};
